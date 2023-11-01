1b40963fd102577133044f05bae15f03545e3f89
KawshikBss <kawshikbss@gmail.com>
2023-11-02 03:14:13 +0600

    completed gallery structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import "./styles/gallery.css";
+
 function App() {
-    return <div>main</div>;
+    return (
+        <div className="container">
+            <div className="container-inner">
+                <div className="top-bar">
+                    <span className="top-bar-fileinfo">
+                        <input
+                            type="checkbox"
+                            id="select-all-btn"
+                        />
+                        3 Files Selected
+                    </span>
+                    <span className="topbar-delete-btn">Delete Files</span>
+                </div>
+                <div className="gallery-wrapper"></div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
