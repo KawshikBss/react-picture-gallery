29e2f23e205d9d05f14eeede96618b5203eb261d
KawshikBss <kawshikbss@gmail.com>
2023-11-02 14:46:37 +0600

    added cards for gallery items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Card from "./components/Card/Card";
 import "./styles/gallery.css";
 
 function App() {
@@ -6,15 +7,18 @@ function App() {
             <div className="container-inner">
                 <div className="top-bar">
                     <span className="top-bar-fileinfo">
-                        <input
-                            type="checkbox"
-                            id="select-all-btn"
-                        />
-                        3 Files Selected
+                        <input type="checkbox" id="select-all-btn" />3 Files
+                        Selected
                     </span>
                     <span className="topbar-delete-btn">Delete Files</span>
                 </div>
-                <div className="gallery-wrapper"></div>
+                <div className="gallery-wrapper">
+                    <div className="gallery-items">
+                        {[...Array(20)].map((segment, index) => (
+                            <Card key={index} firstItem={index === 0} />
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
