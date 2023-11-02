236559b4e1b6fcf4b010b20a4b51ec09b4dcb69f
KawshikBss <kawshikbss@gmail.com>
2023-11-02 18:52:07 +0600

    completed normal gallery functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "./components/Card/Card";
 import "./styles/gallery.css";
 import { BsCardImage } from "react-icons/bs";
 
 function App() {
     const [uploadedFiles, setUploadedFiles] = useState([]);
+    const [selectedFilesCount, setSelectedFilesCount] = useState(0);
     const handleFileUpload = (event) => {
         var file = event?.target?.files?.[0];
-        if (file) setUploadedFiles((prev) => [...prev, file]);
+        if (file)
+            setUploadedFiles((prev) => [
+                ...prev,
+                { file: file, selected: false },
+            ]);
     };
+    const getSelectedFilesCount = () => {
+        if (!uploadedFiles?.length) return 0;
+        return uploadedFiles.filter((item) => item.selected).length;
+    };
+    const handleFileSelect = (file) => {
+        setUploadedFiles((prev) =>
+            prev.map((item) =>
+                item.file === file.file
+                    ? { ...item, selected: !item.selected }
+                    : item
+            )
+        );
+    };
+    const handleSelectAll = (event) => {
+        if (!event.target.checked) return;
+        setUploadedFiles((prev) =>
+            prev.map((item) => {
+                return { ...item, selected: true };
+            })
+        );
+    };
+    const handleFileDelete = () => {
+        setUploadedFiles((prev) =>
+            prev.filter((item) => item.selected !== true)
+        );
+    };
+    useEffect(() => {
+        setSelectedFilesCount(getSelectedFilesCount());
+    }, [uploadedFiles]);
+    console.table(uploadedFiles);
     return (
         <div className="container">
             <div className="container-inner">
                 <div className="top-bar">
                     <span className="top-bar-fileinfo">
-                        <input type="checkbox" id="select-all-btn" />3 Files
-                        Selected
+                        {selectedFilesCount ? (
+                            <>
+                                <input
+                                    type="checkbox"
+                                    id="select-all-btn"
+                                    onChange={handleSelectAll}
+                                />
+                                {selectedFilesCount} Files Selected
+                            </>
+                        ) : (
+                            "Gallery"
+                        )}
                     </span>
-                    <span className="topbar-delete-btn">Delete Files</span>
+                    <span
+                        className="topbar-delete-btn"
+                        onClick={handleFileDelete}
+                    >
+                        {selectedFilesCount
+                            ? selectedFilesCount > 1
+                                ? "Delete Files"
+                                : "Delete File"
+                            : ""}
+                    </span>
                 </div>
                 <div className="gallery-wrapper">
                     <div className="gallery-items">
                         {uploadedFiles?.length
-                            ? uploadedFiles.map((segment, index) => (
-                                  <Card key={index} firstItem={index === 0} />
+                            ? uploadedFiles.map((item, index) => (
+                                  <Card
+                                      key={index}
+                                      firstItem={index === 0}
+                                      item={item}
+                                      handleFileSelect={handleFileSelect}
+                                  />
                               ))
                             : null}
                         <label className="gallery-add-btn">
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import "./card.css";
 
-const Card = ({ firstItem }) => {
+const Card = ({ firstItem, item, handleFileSelect }) => {
     return (
         <div
-            className={`card-container ${firstItem ? "card-first" : ""}`}
-        ></div>
+            className={`card-container ${firstItem ? "card-first" : ""} ${
+                item.selected ? "selected" : ""
+            }`}
+        >
+            <input
+                type="checkbox"
+                className="card-select"
+                onChange={() => handleFileSelect(item)}
+                checked={item?.selected ? true : false}
+            />
+            <div className="card-overlay" />
+            <img
+                src={item ? URL.createObjectURL(item.file) : ""}
+                className="card-img"
+                alt="gallery-img"
+            />
+        </div>
     );
 };
 
